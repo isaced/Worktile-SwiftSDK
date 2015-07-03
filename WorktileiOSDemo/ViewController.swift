c29752cf96ad330719d7d3fae2c6e6d4dcdf2140
isaced <isaced@163.com>
2015-07-03 11:12:28 +0800

    update iOS demo

--- WorktileiOSDemo/ViewController.swift
+++ WorktileiOSDemo/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import WorktileiOS
 
-class ViewController: UIViewController , WorktileDelegate {
+class ViewController: UITableViewController , WorktileDelegate {
 
     let worktile = Worktile(appKey: "eef4247ee75c4eeba9946900f9579688")
+    
+    var teamID: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +25,8 @@ class ViewController: UIViewController , WorktileDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func login(sender: UIButton) {
-        let authorizeVC = worktile.authorizeViewController()
-        let nav = UINavigationController(rootViewController: authorizeVC)
-        self.presentViewController(nav, animated: true, completion: nil)
-    }
 
+    // Login Delegate
     func authorizeComplate(currentAuthorizeViewController: UIViewController, success: Bool) {
         currentAuthorizeViewController.dismissViewControllerAnimated(true, completion: nil)
         if success == true {
@@ -41,17 +38,52 @@ class ViewController: UIViewController , WorktileDelegate {
     
     // MARK: -
     
-    @IBAction func refreshToken(sender: UIButton) {
-        // 刷新 Token
-        worktile.toRefreshToken()
-        print("refresh token.")
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        switch indexPath.section {
+        case 0:
+            switch indexPath.row {
+            case 0: // 登录
+                let authorizeVC = worktile.authorizeViewController()
+                let nav = UINavigationController(rootViewController: authorizeVC)
+                self.presentViewController(nav, animated: true, completion: nil)
+            case 1: // 刷新 Token
+                worktile.toRefreshToken()
+                print("refresh token.")
+            default: print("-")
+            }
+        case 1: // 用户
+            worktile.profile({ (response) -> Void in
+                print("profile: \(response)")
+            })
+        case 2: // 团队
+            switch indexPath.row {
+            case 0: // 团队列表
+                worktile.teams({ (response) -> Void in
+                    print(response)
+                    
+                    // 取一个团队ID后面用
+                    if let response = response as? Array<Dictionary<String,AnyObject>> {
+                        if let firstTeam = response.first {
+                            self.teamID = firstTeam["team_id"] as? String
+                        }
+                    }
+                    
+                })
+            case 1: // 团队信息
+                if let teamID = self.teamID {
+                    worktile.teamInfo(teamID, finishCallback: { (response) -> Void in
+                        print(response)
+                    })
+                }
+            default: print("-")
+            }
+        default:print("~")
+        }
     }
     
-    @IBAction func getUserInfo(sender: UIButton) {
-        // 获取用户信息
-        worktile.profile({ (response) -> Void in
-            print("profile: \(response)")
-        })
-    }
+    //
+
 }
 
